dc170578a7e7aa5b8a6c3c4c0f14a58cdc61c797
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-01-31 07:54:57 +0300

    Worked on react-bootstrap Container

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import ThemeProvider from "react-bootstrap/ThemeProvider";
+import PageFooter from "./Components/PageFooter";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <ThemeProvider
+            breakpoints={['xxl', 'xl', 'lg', 'md', 'sm', 'xs', 'xxs']}
+            minBreakpoint="xxs"
         >
-          Learn React
-        </a>
-      </header>
+           <PageFooter></PageFooter>
+        </ThemeProvider>
     </div>
   );
 }
---
+++ src/Components/PageFooter.js
@@ -0,0 +1,17 @@
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+const PageFooter = () =>
+{
+    return(<>
+
+        <Container fluid={"xxl"}>
+            <Row>
+                <Col color={"primary"}>1 of 20</Col>
+            </Row>
+        </Container>
+
+        </>)
+}
+
+export default PageFooter;
