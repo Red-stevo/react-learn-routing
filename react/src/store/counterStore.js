a31735ac533ea7a5550e583e80c10e2aa26d90dc
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-05-25 15:37:55 +0300

    Commit message >>>>> set up the store and configured a reducer. <<<<<

---
+++ react/src/store/counterStore.js
@@ -0,0 +1,17 @@
+import {createSlice} from "@reduxjs/toolkit";
+
+export const initialState = {
+    count : 0
+}
+
+export const counter = createSlice({
+    name:"counter",
+    initialState,
+    reducers:{
+        increment:(state) => { state.count += 1;},
+        decrement:(state) => {state.count-=1;},
+        changeByAmount:(state, action) => {state.count += action.payload;},
+        reset:(state) => {state.count = 0;}
+
+    }
+})
