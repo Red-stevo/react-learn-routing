ff4a6f2746f0c9098647fd489b30489195258f5f
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-06-27 21:41:38 +0300

    Commit message >>>>> done <<<<<

--- redux/src/App.jsx
+++ redux/src/App.jsx
@@ -1,9 +1,12 @@
 import './App.css'
+import {Route, Routes} from "react-router-dom";
 
 function App() {
   return (
-    <>
-    </>
+    <Routes>
+      <Route index element={<h1>Hello index page</h1>}>
+      </Route>
+    </Routes>
   )
 }
 
