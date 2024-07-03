ca21c7cade52d55eeae88ad5f4c93094211a038e
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-07-03 09:26:31 +0300

    Commit message >>>>> cors errors <<<<<

--- redux-tool/src/Containers/ProductContains/ProductsDisplay.jsx
+++ redux-tool/src/Containers/ProductContains/ProductsDisplay.jsx
@@ -1,18 +1,43 @@
 import {Button, Card} from "react-bootstrap";
+import {useDispatch, useSelector} from "react-redux";
+import {useEffect} from "react";
+import {fetchProducts} from "../BackendUtils/FetchProducts.js";
+import {setProducts} from "../../redux/actions/productsActions.js";
+
 
 const ProductsDisplay = () => {
+    const products = useSelector((state) => state.productsState.products);
+    const dispatch = useDispatch();
+
+    const saveProducts = () => {
+        fetchProducts().then((res) => {
+            dispatch(setProducts(res.data));
+            console.log(res)
+        }).catch(error => {
+            console.log(error.response)
+        })
+    }
+
+
+    useEffect(() => {
+        console.log(products);
+        saveProducts();
+    }, []);
+
     return(<>
-        <Card style={{ width: '18rem' }} className={"mt-3"}>
-        <Card.Img variant="top" src="holder.js/200px200" height={200} width={200} />
-        <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Text>
-                Some quick example text to build on the card title and make up the
-                bulk of the card's content.
-            </Card.Text>
-            <Button variant="primary">View</Button>
-        </Card.Body>
-        </Card>
+        {products.length > 0? <Card style={{ width: '18rem' }} className={"mt-3"}>
+            <Card.Img variant="top" src="holder.js/200px200" height={200} width={200} />
+            <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                    Some quick example text to build on the card title and make up the
+                    bulk of the card's content.
+                </Card.Text>
+                <Button variant="primary">View</Button>
+            </Card.Body>
+        </Card>:
+        <h2 className={"display-6 justify-content-center align-content-center flex-column"}>
+            There Are No Products At The Moment?</h2>}
     </>);
 }
 
