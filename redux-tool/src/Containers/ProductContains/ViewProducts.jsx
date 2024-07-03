fee7ada3c6d9ec793dd9252f07391258a2bb1f10
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-07-03 20:38:49 +0300

    Commit message >>>>> added the pricing <<<<<

--- redux-tool/src/Containers/ProductContains/ViewProducts.jsx
+++ redux-tool/src/Containers/ProductContains/ViewProducts.jsx
@@ -9,13 +9,20 @@ const ViewProducts = () => {
             {(products.length > 0) ?
                 products.map((record) => {
                     return (
-                        <Button className="product-col" key={record.id} onClick={() => console.error("we are doing great.")}>
+                        <Button className="product-col" key={record.id}
+                                onClick={() => console.error("we are doing great.")}>
                             <Card id="product-card">
                                 <Card.Img variant="top" src={record.image} id="product"/>
                                 <Card.Body id="card-body">
-                                    <Card.Title id="title">{record.title}</Card.Title>
+                                    <Card.Title id="title">{record.title.substring(0, 35)+"..."}</Card.Title>
+                                    <Card.Text className={"card-text"}>
+                                        <span className={"price"}>${record.price}</span><br/>
+                                        <span className={"category"}>{record.category}</span>
+                                    </Card.Text>
                                 </Card.Body>
+
                             </Card>
+
                         </Button>
                     );
                 }) :
