aa6e44b1ea272b8deec6ace29df2c31938d902d8
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-07-03 08:07:29 +0300

    Commit message >>>>> products display <<<<<

--- redux-tool/src/App.jsx
+++ redux-tool/src/App.jsx
@@ -1,13 +1,14 @@
 import './App.css'
 import {BrowserRouter as Router, Routes, Route}  from "react-router-dom";
 import HomeHeader from "./Containers/ProductContains/HomeHeader.jsx";
+import ProductsDisplay from "./Containers/ProductContains/ProductsDisplay.jsx";
 
 function App() {
   return (
       <Router>
           <Routes>
               <Route path={"/home"} element={<HomeHeader />} >
-
+                <Route path={"shop"} element={<ProductsDisplay />}/>
               </Route>
           </Routes>
       </Router>
---
+++ redux-tool/src/Containers/ProductContains/ProductsDisplay.jsx
@@ -0,0 +1,19 @@
+import {Button, Card} from "react-bootstrap";
+
+const ProductsDisplay = () => {
+    return(<>
+        <Card style={{ width: '18rem' }} className={"mt-3"}>
+        <Card.Img variant="top" src="holder.js/200px200" height={200} width={200} />
+        <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>
+                Some quick example text to build on the card title and make up the
+                bulk of the card's content.
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+        </Card>
+    </>);
+}
+
+export default ProductsDisplay;
