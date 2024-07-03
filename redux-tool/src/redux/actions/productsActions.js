801dba5212249dd06d13ae043b1bcfc885f94197
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-07-03 06:14:55 +0300

    Commit message >>>>> configured the reducer <<<<<

--- redux-tool/src/redux/actions/productsActions.js
+++ redux-tool/src/redux/actions/productsActions.js
@@ -3,17 +3,13 @@ import {productsActionsTypes} from "./actionTypes/productsActionsTypes.js";
 export const setProducts = (products) => {
     return {
         type:productsActionsTypes.SET_PRODUCTS,
-        payload: products
+        payload: products,
     }
 }
 
-export const selectProduct = ({name, imageUrl, price}) => {
+export const selectProduct = (product) => {
     return {
         type: productsActionsTypes.SELECTED_PRODUCT,
-        payload: {
-            name,
-            imageUrl,
-            price,
-        }
+        payload:product,
     }
 }
---
+++ redux-tool/src/redux/reducers/productsReducers.js
@@ -0,0 +1,14 @@
+import {productsActionsTypes} from "../actions/actionTypes/productsActionsTypes.js";
+
+const initialState = {
+    products:[{
+        name:"stephen",
+        image:"https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.4giKiPRejlKdfsyw8tJf_AHaEo%26pid%3DApi&f=1&ipt=50a93954b0967c45231fc3579ea38fa2ab8cd3f1b5556265e8a455eac53eeec8&ipo=images"
+    }]
+}
+export const productsReducer = (initialState, {type, payload}) =>{
+    if(type === productsActionsTypes.SET_PRODUCTS){
+        return [...initialState, ...payload]
+    }else
+        return initialState;
+}
