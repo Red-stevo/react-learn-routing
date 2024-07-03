20443ff683e282afd2244496517be478550c760a
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-07-03 13:07:31 +0300

    Commit message >>>>> design is comming up nicely <<<<<

--- redux-tool/src/Containers/ProductContains/ProductsDisplay.jsx
+++ redux-tool/src/Containers/ProductContains/ProductsDisplay.jsx
@@ -1,8 +1,9 @@
-import {Button, Card, Spinner} from "react-bootstrap";
+import {Button, Card, Col, Container, Spinner} from "react-bootstrap";
 import {useDispatch, useSelector} from "react-redux";
 import {useEffect} from "react";
 import {fetchProducts} from "../BackendUtils/FetchProducts.js";
 import {setProducts} from "../../redux/actions/productsActions.js";
+import "./../../Styling/displayProduct.css"
 
 
 const ProductsDisplay = () => {
@@ -23,23 +24,22 @@ const ProductsDisplay = () => {
         saveProducts();
     }, []);
 
-    return(<>
-        {products.length > 0?products.map((record) => {
+    return(<div  id={"display-page"}>
+        {(products.length > 0) ?
+            products.map((record) => {
             return(
-                <Card  className={"mt-3"} key={record.id}>
-                    <Card.Img variant="top" src={record.image} id={"product"} />
-                    <Card.Body>
-                        <Card.Title>{record.title}</Card.Title>
-                        <Card.Text>
-                            {record.description}
-                        </Card.Text>
-                        <Button variant="primary">View</Button>
-                    </Card.Body>
-                </Card>
+                <Col key={record.id} className={"col-lg-3"}>
+                    <Card  className={"mt-3"} id={"product-card"}>
+                        <Card.Img variant="top" src={record.image} id={"product"} />
+                        <Card.Body id={"card-body"}>
+                            <Card.Title id={"title"}>{record.title}</Card.Title>
+                        </Card.Body>
+                    </Card>
+                </Col>
             )
             }):
         <h2>Loading... <Spinner animation="border" /></h2>}
-    </>);
+    </div>);
 }
 
 export default ProductsDisplay;
