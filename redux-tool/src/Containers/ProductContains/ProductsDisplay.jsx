481fd6f095a4b455845cde3bbd79cae01c3221cd
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-07-03 14:06:37 +0300

    Commit message >>>>> changed the layout design <<<<<

--- redux-tool/src/Containers/ProductContains/ProductsDisplay.jsx
+++ redux-tool/src/Containers/ProductContains/ProductsDisplay.jsx
@@ -2,7 +2,6 @@ import {useDispatch} from "react-redux";
 import {useEffect} from "react";
 import {fetchProducts} from "../BackendUtils/FetchProducts.js";
 import {setProducts} from "../../redux/actions/productsActions.js";
-import "./../../Styling/displayProduct.css"
 import ViewProducts from "./ViewProducts.jsx";
 
 
@@ -21,12 +20,7 @@ const ProductsDisplay = () => {
         saveProducts();
     }, []);
 
-    return(<div>
-        <ViewProducts />
-        <ViewProducts />
-        <ViewProducts />
-        <ViewProducts />
-        </div>);
+    return(<ViewProducts />);
 }
 
 export default ProductsDisplay;
--- redux-tool/src/Containers/ProductContains/ViewProducts.jsx
+++ redux-tool/src/Containers/ProductContains/ViewProducts.jsx
@@ -1,26 +1,26 @@
-import {Card, Col, Spinner} from "react-bootstrap";
+import {Card, Spinner} from "react-bootstrap";
 import {useSelector} from "react-redux";
+import "./../../Styling/displayProduct.css"
 
 const ViewProducts = () => {
     const products = useSelector((state) => state.productsState.products);
-    return(
-        <div id={"display-page"}>
+    return (
+        <div id="display-page" className="product-container">
             {(products.length > 0) ?
                 products.map((record) => {
-                    return(
-                        <Col key={record.id} className={"col-lg-3"}>
-                            <Card  className={"mt-3"} id={"product-card"}>
-                                <Card.Img variant="top" src={record.image} id={"product"} />
-                                <Card.Body id={"card-body"}>
-                                    <Card.Title id={"title"}>{record.title}</Card.Title>
+                    return (
+                        <div className="product-col" key={record.id}>
+                            <Card id="product-card">
+                                <Card.Img variant="top" src={record.image} id="product"/>
+                                <Card.Body id="card-body">
+                                    <Card.Title id="title">{record.title}</Card.Title>
                                 </Card.Body>
                             </Card>
-                        </Col>
-                    )
-                }):
-                <h2>Loading... <Spinner animation="border" /></h2>}
-        </div>
-    )
+                        </div>
+                    );
+                }) :
+                <h2>Loading... <Spinner animation="border"/></h2>}
+        </div>);
 }
 
 export default ViewProducts;
