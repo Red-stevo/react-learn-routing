2f633d624087afb7a98ed4c68a1f2e81ce44f1b7
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-07-03 07:23:07 +0300

    Commit message >>>>> store configured <<<<<

--- redux-tool/src/redux/reducers/index.js
+++ redux-tool/src/redux/reducers/index.js
@@ -2,6 +2,6 @@ import {combineReducers} from "redux";
 import {productsReducer} from "./productsReducers.js";
 
 
-export const AllProduct = combineReducers({
+export const AllProducts = combineReducers({
     productsState:productsReducer,
 });
--- redux-tool/src/redux/reducers/store.js
+++ redux-tool/src/redux/reducers/store.js
@@ -1,4 +1,5 @@
 import {createStore} from "redux";
-import {productsReducer} from "./productsReducers.js";
+import { AllProducts} from "./index.js";
 
-export const  store = createStore(productsReducer);
+export const  store = createStore(AllProducts,{},
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
