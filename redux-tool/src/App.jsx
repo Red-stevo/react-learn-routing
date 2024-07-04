ceb3d35d95e16723121bc7cd6db6a4cc66732994
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-07-04 20:56:32 +0300

    Commit message >>>>> configured the variable url <<<<<

--- redux-tool/src/App.jsx
+++ redux-tool/src/App.jsx
@@ -2,13 +2,15 @@ import './App.css'
 import {BrowserRouter as Router, Routes, Route}  from "react-router-dom";
 import HomeHeader from "./Containers/ProductContains/HomeHeader.jsx";
 import ProductsDisplay from "./Containers/ProductContains/ProductsDisplay.jsx";
+import ProductDetails from "./Containers/ProductContains/ProductDetails.jsx";
 
 function App() {
   return (
       <Router>
           <Routes>
               <Route path={"home"} element={<HomeHeader />} >
-                <Route path={"shop"} element={<ProductsDisplay />}/>
+                  <Route path={"shop"} element={<ProductsDisplay />}/>
+                  <Route path={"product/:productId"} element={<ProductDetails />} />
               </Route>
           </Routes>
       </Router>
